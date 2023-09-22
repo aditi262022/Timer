b84e2fca0708a9d4d316bf2ac5d1262d70d32228
Aditi Nigam <116663229+aditi262022@users.noreply.github.com>
2023-09-22 19:54:21 +0530

    Add files via upload

---
+++ Timer.c
@@ -0,0 +1,535 @@
+//build a timer with numbers showing up in pattern '*'
+
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+void main()
+{
+	int i, j, k;
+	printf("\n");
+	printf("\n");
+	for(i=0;i<=9;i++)
+	{
+		printf("      ");
+		for(j=1;j<=1;j++)
+		{
+			printf("*", j);
+		}
+		printf("*\n", i);
+	}
+	
+	Sleep(2000);
+	system("cls");
+	
+	printf("\n");
+	printf("\n");
+	for(i=0;i<=7;i++)
+	{
+		printf("*", i);		
+	}
+	printf("\n");
+	for(j=0;j<=7;j++)
+	{
+		printf("*", j);	
+	}
+	printf("\n");
+	printf("      ");
+	for(k=0;k<=1;k++)
+	{
+		printf("*", k);	
+	}
+	printf("\n");
+	printf("      ");
+	for(k=0;k<=1;k++)
+	{
+		printf("*", k);	
+	}
+	printf("\n");
+	for(i=0;i<=7;i++)
+	{
+		printf("*", i);		
+	}
+	printf("\n");
+	for(j=0;j<=7;j++)
+	{
+		printf("*", j);	
+	}
+	printf("\n");
+	for(k=0;k<=1;k++)
+	{
+		printf("*", k);	
+	}
+	printf("\n");
+	for(k=0;k<=1;k++)
+	{
+		printf("*", k);	
+	}
+	printf("\n");
+	for(i=0;i<=7;i++)
+	{
+		printf("*", i);		
+	}
+	printf("\n");
+	for(j=0;j<=7;j++)
+	{
+		printf("*", j);	
+	}
+	
+	
+	Sleep(2000);
+	system("cls");
+	
+	printf("\n");	
+	printf("\n");	
+	for(i=0;i<=7;i++)
+	{
+		printf("*", i);		
+	}
+	printf("\n");
+	for(j=0;j<=7;j++)
+	{
+		printf("*", j);	
+	}
+	printf("\n");
+	printf("      ");
+	for(k=0;k<=1;k++)
+	{
+		printf("*", k);	
+	}
+	printf("\n");
+	printf("      ");
+	for(k=0;k<=1;k++)
+	{
+		printf("*", k);	
+	}
+	printf("\n");
+	for(i=0;i<=7;i++)
+	{
+		printf("*", i);		
+	}
+	printf("\n");
+	for(j=0;j<=7;j++)
+	{
+		printf("*", j);	
+	}
+	printf("\n");
+	printf("      ");
+	for(k=0;k<=1;k++)
+	{
+		printf("*", k);	
+	}
+	printf("\n");
+	printf("      ");
+	for(k=0;k<=1;k++)
+	{
+		printf("*", k);	
+	}
+	printf("\n");
+	for(i=0;i<=7;i++)
+	{
+		printf("*", i);		
+	}
+	printf("\n");
+	for(j=0;j<=7;j++)
+	{
+		printf("*", j);	
+	}
+	
+	Sleep(2000);
+	system("cls");
+	
+	printf("\n");
+	printf("\n");	
+	for(i=0;i<=3;i++)
+	{
+	
+		for(j=1;j<=1;j++)
+		{
+			
+			printf("**", j);
+		}
+			printf("    ");
+		printf("**\n", i);
+	}
+	
+	for(i=0;i<=7;i++)
+	{
+		printf("*", i);		
+	}
+	printf("\n");
+	for(j=0;j<=7;j++)
+	{
+		printf("*", j);	
+	}
+	printf("\n");
+	for(j=0;j<=3;j++)
+		{
+			printf("      ");
+			printf("**\n", j);
+		}
+	
+	
+	Sleep(2000);
+	system("cls");
+		
+	printf("\n");
+	printf("\n");	
+	for(i=0;i<=7;i++)
+	{
+		printf("*", i);		
+	}
+	printf("\n");
+	for(j=0;j<=7;j++)
+	{
+		printf("*", j);	
+	}
+	printf("\n");
+	for(k=0;k<=1;k++)
+	{
+		printf("*", k);	
+	}
+	printf("\n");
+	for(k=0;k<=1;k++)
+	{
+		printf("*", k);	
+	}
+	printf("\n");
+	for(i=0;i<=7;i++)
+	{
+		printf("*", i);		
+	}
+	printf("\n");
+	for(j=0;j<=7;j++)
+	{
+		printf("*", j);	
+	}
+	printf("\n");
+	printf("      ");
+	for(k=0;k<=1;k++)
+	{
+		printf("*", k);	
+	}
+	printf("\n");
+	printf("      ");
+	for(k=0;k<=1;k++)
+	{
+		printf("*", k);	
+	}
+	printf("\n");
+	for(i=0;i<=7;i++)
+	{
+		printf("*", i);		
+	}
+	printf("\n");
+	for(j=0;j<=7;j++)
+	{
+		printf("*", j);	
+	}
+	
+	
+	Sleep(2000);
+	system("cls");
+	
+	printf("\n");
+	printf("\n");
+	for(i=0;i<=7;i++)
+	{
+		printf("*", i);		
+	}
+	printf("\n");
+	for(j=0;j<=7;j++)
+	{
+		printf("*", j);	
+	}
+	printf("\n");
+	for(i=0;i<=1;i++)
+	{
+	
+		for(j=1;j<=1;j++)
+		{
+			
+			printf("*", j);
+		}
+		printf("*\n", i);
+	}
+	
+	for(i=0;i<=7;i++)
+	{
+		printf("*", i);		
+	}
+	printf("\n");
+	for(j=0;j<=7;j++)
+	{
+		printf("*", j);	
+	}
+	printf("\n");
+	for(i=0;i<=1;i++)
+	{
+	
+		for(j=1;j<=1;j++)
+		{
+			
+			printf("**", j);
+		}
+			printf("    ");
+		printf("**\n", i);
+	}
+	for(i=0;i<=7;i++)
+	{
+		printf("*", i);		
+	}
+	printf("\n");
+	for(j=0;j<=7;j++)
+	{
+		printf("*", j);	
+	}
+	
+	
+	Sleep(2000);
+	system("cls");
+	
+	printf("\n");
+	printf("\n");
+		for(i=0;i<=7;i++)
+	{
+		printf("*", i);		
+	}
+	printf("\n");
+	for(j=0;j<=7;j++)
+	{
+		printf("*", j);	
+	}
+	printf("\n");
+	
+	for(k=0;k<=7;k++)
+	{
+		printf("      ");
+		printf("**\n", k);	
+	}
+	
+	Sleep(2000);
+	system("cls");
+	
+	
+	printf("\n");
+	printf("\n");
+	for(i=0;i<=7;i++)
+	{
+		printf("*", i);		
+	}
+	printf("\n");
+	for(j=0;j<=7;j++)
+	{
+		printf("*", j);	
+	}
+	printf("\n");
+	for(k=1;k<=1;k++)
+	{
+		printf("**    **", k);	
+	}
+	printf("\n");
+	for(k=1;k<=1;k++)
+	{
+		printf("**    **", k);	
+	}
+	printf("\n");
+	for(i=0;i<=7;i++)
+	{
+		printf("*", i);		
+	}
+	printf("\n");
+	for(j=0;j<=7;j++)
+	{
+		printf("*", j);	
+	}
+	printf("\n");
+	for(k=1;k<=1;k++)
+	{
+		printf("**    **", k);	
+	}
+	printf("\n");
+	for(k=1;k<=1;k++)
+	{
+		printf("**    **", k);	
+	}
+	printf("\n");
+	for(i=0;i<=7;i++)
+	{
+		printf("*", i);		
+	}
+	printf("\n");
+	for(j=0;j<=7;j++)
+	{
+		printf("*", j);	
+	}
+	
+	
+	Sleep(2000);
+	system("cls");
+	
+	printf("\n");
+	printf("\n");
+	for(i=0;i<=7;i++)
+	{
+		printf("*", i);		
+	}
+	printf("\n");
+	for(j=0;j<=7;j++)
+	{
+		printf("*", j);	
+	}
+	printf("\n");
+	for(k=1;k<=1;k++)
+	{
+		printf("**    **", k);	
+	}
+	printf("\n");
+	for(k=1;k<=1;k++)
+	{
+		printf("**    **", k);	
+	}
+	printf("\n");
+	for(i=0;i<=7;i++)
+	{
+		printf("*", i);		
+	}
+	printf("\n");
+	for(j=0;j<=7;j++)
+	{
+		printf("*", j);	
+	}
+	printf("\n");
+	printf("      ");
+	for(k=0;k<=1;k++)
+	{
+		printf("*", k);	
+	}
+	printf("\n");
+	printf("      ");
+	for(k=0;k<=1;k++)
+	{
+		printf("*", k);	
+	}
+	printf("\n");
+	for(i=0;i<=7;i++)
+	{
+		printf("*", i);		
+	}
+	printf("\n");
+	for(j=0;j<=7;j++)
+	{
+		printf("*", j);	
+	}
+	printf("\n");
+	printf("\n");
+	
+	
+	Sleep(2000);
+	system("cls");
+	
+	COORD c;
+	c.X = 0;
+	c.Y = 1;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+	printf("\n");
+	for(k=0;k<=9;k++)
+	{
+		printf("**\n", k);	
+	}
+	
+	c.X = 6;
+	c.Y = 2;
+	
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+	
+	for(i=0;i<=7;i++)
+	{
+		printf("*", i);		
+	}
+	printf("\n");
+	
+	
+	c.X = 6;
+	c.Y = 3;
+	
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+	for(j=0;j<=7;j++)
+	{
+		printf("*", j);	
+	}
+	printf("\n");
+	
+	c.X = 6;
+	c.Y = 4;
+	
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+	for(i=1;i<=1;i++)
+	{
+		printf("**    **\n", i);
+	}
+	c.X = 6;
+	c.Y = 5;
+	
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+	for(i=1;i<=1;i++)
+	{
+		printf("**    **\n", i);
+	}
+	c.X = 6;
+	c.Y = 6;
+	
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+	for(i=1;i<=1;i++)
+	{
+		printf("**    **\n", i);
+	}
+	c.X = 6;
+	c.Y = 7;
+	
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+	for(i=1;i<=1;i++)
+	{
+		printf("**    **\n", i);
+	}
+	c.X = 6;
+	c.Y = 8;
+	
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+	for(i=1;i<=1;i++)
+	{
+		printf("**    **\n", i);
+	}
+	c.X = 6;
+	c.Y = 9;
+	
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+	for(i=1;i<=1;i++)
+	{
+		printf("**    **\n", i);
+	}
+	
+	c.X = 6;
+	c.Y = 10;
+	
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+	
+	for(i=0;i<=7;i++)
+	{
+		printf("*", i);		
+	}
+	printf("\n");
+	
+	
+	c.X = 6;
+	c.Y = 11;
+	
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+	for(j=0;j<=7;j++)
+	{
+		printf("*", j);	
+	}
+	printf("\n");
+	
+    getch();
+
+}
